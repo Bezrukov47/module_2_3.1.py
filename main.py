5921b1939c50bcd79922e9830011a18aad56d2f2
Bezrukov47 <bezr47@gmail.com>
2024-11-27 10:25:46 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-my_list: int = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-print (my_list)
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 index = 0
 while index < len(my_list):
-   index += 1
-   if my_list[index] < 0:
+    index += 1
+    if my_list[index] < 0:
        break
-   if my_list[index] > 0:
-       print (my_list[index])
+    elif my_list[index] == 0:
        continue
-   if my_list[index] == -6:
-       break
+    elif my_list[index] > 0:
        print (my_list[index])
